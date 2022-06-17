37ee2afbb807c8419bccd4bef8bfbef49d32a0ed
Kelvin Hsu <skps0102@gmail.com>
2022-06-17 22:55:39 +0800

    示範 內嵌繫結(interpolation) & 屬性繫結(Property Binding)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  siteName = 'Hello World';
+  subTitle = '記載著 Will 在網路世界的學習心得與技術分享';
+  logoId = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.siteName = 'The Will Will Web';
+    }, 2000)
   }
 
 }
