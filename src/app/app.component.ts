b0c6a047e2063b284dc2abd3f8ad2a649a200c1f
Kelvin Hsu <skps0102@gmail.com>
2022-06-18 13:35:53 +0800

    示範 HttpClient 初始化與基本 HTTP GET 的用法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   keyword = 'Angular';
+  data: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.http.get<any[]>('/api/articles.json').subscribe({
+      next: (result) => {
+        this.data = result;
+        console.log(this.data);
+      }
+    })
+  }
 
   doSearch(tKeyword: HTMLInputElement, str: string) {
     this.keyword = tKeyword.value = str;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -14,7 +16,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
