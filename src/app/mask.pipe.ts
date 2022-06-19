1599dc704fa6d77e9ac87f225446231b209cb1c6
Kelvin Hsu <skps0102@gmail.com>
2022-06-19 11:40:35 +0800

    ng g pipe mask

---
+++ src/app/mask.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'mask'
+})
+export class MaskPipe implements PipeTransform {
+
+  transform(value: string, len : number = 3): string {
+    return value.substring(0, len) + '****' + value.substring(value.length - len, 10);
+  }
+
+}
